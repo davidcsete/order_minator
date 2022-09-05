73855938aaf21761a4edebe54c571d318d317666
David Csete <davidcsetefogel@gmail.com>
2022-09-05 16:13:07 +0200

    initial commit

---
+++ config/importmap.rb
@@ -0,0 +1,8 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "vue" # @3.2.29
+pin "@vue/reactivity", to: "@vue--reactivity.js" # @3.2.29
+pin "@vue/runtime-core", to: "@vue--runtime-core.js" # @3.2.29
+pin "@vue/runtime-dom", to: "@vue--runtime-dom.js" # @3.2.29
+pin "@vue/shared", to: "@vue--shared.js" # @3.2.29
